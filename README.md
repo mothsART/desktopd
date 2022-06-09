ad08a248af84f7d5542d1624a30f2444ed835236
Ferry Jérémie <ferryjeremie@free.fr>
2022-06-09 23:11:30 +0200

    parsing all desktop files on XDG_DATA_DIRS

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ A linux daemon who store .desktop meta-datas from $XDG_DATA_DIRS on database.
 
 ## Goal
 
-Give the best API
+Give the best API to find applications store in $XDG_DATA_DIRS
--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,15 @@ use crate::collect::{collect_desktops_path, get_dirs};
 use crate::desktop::DesktopFile;
 
 fn main() {
-    let d = DesktopFile::new(Path::new("tests/test1/path-1/applications/app-one.desktop"));
-    println!("{:?}", d);
-    /*
     let xdg_data_dirs = env::var("XDG_DATA_DIRS");
 
     match xdg_data_dirs {
         Ok(v) => {
             let dirs = get_dirs(&v);
-            println!("{:?}", dirs);
-            println!("{:?}", collect_desktops_path(dirs));
+            for path in collect_desktops_path(dirs) {
+                let d = DesktopFile::new(Path::new(&path));
+            }
         }
         Err(e) => eprintln!("Error: {}", e),
-    }*/
+    }
 }
